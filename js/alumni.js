b041b7494087ef8750c1f3a01e7c25487eb260f7
Saket Sarin <sarinsaket@gmail.com>
2023-03-21 14:59:02 +0530

    alumni page

---
+++ js/alumni.js
@@ -0,0 +1,17 @@
+// scroll the invitation video with the page only on big screens
+var alumniIntro = document.getElementById("alumniIntro");
+alumniIntro.style.position = "relative";
+
+window.addEventListener("scroll", function () {
+  var value = window.scrollY - 50;
+
+  if (window.innerWidth > 768) {
+    if (value < 0) {
+      value = 0;
+    } else if (value > 500) {
+      value = 500;
+    }
+
+    alumniIntro.style.top = value * 0.75 + "px";
+  }
+});
